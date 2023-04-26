e5f824ba1712296fcc04a778c2a935e80427deb0
Kullanıcı Adınız <email@adresiniz.com>
2023-04-26 12:43:19 +0300

    başlangıçtak yüklenmesi gereken ilk 10 gemi listelendi

--- src/components/From/Form.jsx
+++ src/components/From/Form.jsx
@@ -1,27 +1,41 @@
 import axios from "axios";
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import SwContext from "../../context/SwContext";
 
 export default function Form() {
   const { sw, setSw, info, setInfo } = useContext(SwContext);
+  const [more, setMore] = useState(10);
+
+  //modele ve ada göre getirme
+  // useEffect(() => {
+  //   if (!info) {
+  //     return;
+  //   }
+  //   axios.get(`https://swapi.dev/api/starships/?search=${info}`).then((res) => {
+  //     setSw(res.data.results);
+  //     setInfo("");
+  //   });
+  // }, [info]);
 
   useEffect(() => {
     if (!info) {
       return;
     }
-    axios.get(`https://swapi.dev/api/starships/?search=${info}`).then((res) => {
-      setSw(res.data.results);
+    axios.get(`https://swapi.dev/api/starships/`).then((res) => {
+      const firstTenShips = res.data.results.slice(0, more);
+      setSw(firstTenShips);
       setInfo("");
     });
-  }, [info]);
+  }, []);
 
   console.log(sw);
-
+  console.log(more);
   const formSubmit = (e) => {
     e.preventDefault();
     if (!info) {
       return;
     }
+    //modele ve ada göre getirme
     axios.get(`https://swapi.dev/api/starships/?search=${info}`).then((res) => {
       setSw(res.data.results);
       setInfo("");
@@ -41,6 +55,8 @@ export default function Form() {
             onChange={(e) => setInfo(e.target.value)}
           />
         </label>
+
+        <button onClick={() => setMore((prev) => prev + 3)}>More</button>
       </form>
     </div>
   );
--- src/context/SwContext.jsx
+++ src/context/SwContext.jsx
@@ -4,6 +4,7 @@ const SwContext = createContext();
 export const SwContextProvider = ({ children }) => {
   const [sw, setSw] = useState([]);
   const [info, setInfo] = useState("Star Destroyer");
+
   const values = { sw, setSw, info, setInfo };
   return <SwContext.Provider value={values}>{children}</SwContext.Provider>;
 };
